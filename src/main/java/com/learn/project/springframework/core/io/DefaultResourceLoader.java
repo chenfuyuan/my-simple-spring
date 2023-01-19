8ce0208de06e2b162a023587488e462e60e54915
chenfuyuan <chenfuyuan0713@163.com>
2023-01-19 14:24:02 +0800

    feat: 补充资源加载和解析章节 笔记文档

--- src/main/java/com/learn/project/springframework/core/io/DefaultResourceLoader.java
+++ src/main/java/com/learn/project/springframework/core/io/DefaultResourceLoader.java
@@ -17,9 +17,11 @@ public class DefaultResourceLoader implements ResourceLoader {
     public Resource getResource(String location) {
         Assert.notNull(location,"Location must not be null");
         if (location.startsWith(CLASSPATH_URL_PREFIX)) {
+            //前缀为 classpath: 时，使用ClassPathResource获取资源
             return new ClassPathResource(location.substring(CLASSPATH_URL_PREFIX.length()));
         }
         try {
+            //先使用URL形式获取，如果抛出异常，再使用FileSystem形式获取
             URL url = new URL(location);
             return new UrlResource(url);
         } catch (MalformedURLException e) {
